78c66318407642c53d5e3fa29f05183c23fde4cd
Fra2000 <fra0000@gmail.com>
2024-05-23 09:30:46 +0200

    d3

--- github/week9/1-4/giorno_3/esercizio/src/app/components/home/home.component.ts
+++ github/week9/1-4/giorno_3/esercizio/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Posts } from '../../models/posts';
+import { Jsonc } from '../../models/json';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+postArr:Posts[]=[]
+firstpost!:Posts
+
+ngOnInit(){
+this.getPosts().then(()=>{
+let firstpost=this.getfirstpost()
+
+if(firstpost) this.firstpost=firstpost
+})
+}
+async getPosts(){
+  const response=await fetch("../../../assets/db.json")
+  const thepost=<Jsonc> await response.json()
+  this.postArr=thepost.posts
+
+}
+
+getfirstpost(){
+  return this.postArr.shift()
+}
+
+
 
 }
