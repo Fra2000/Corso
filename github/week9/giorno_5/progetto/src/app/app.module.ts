3c6712e7e223389a5a479158c2874828f00de4a0
Fra2000 <fra0000@gmail.com>
2024-05-24 14:31:38 +0200

    d5

---
+++ github/week9/giorno_5/progetto/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Page404Component } from './pages/page404/page404.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FiatComponent,
+    FordComponent,
+    AudiComponent,
+    HeaderComponent,
+    FooterComponent,
+    Page404Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
