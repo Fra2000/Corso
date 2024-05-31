58a0cf7591b286ecdbfd95fdf831412aa44a5ace
Fra2000 <fra0000@gmail.com>
2024-05-31 16:58:30 +0200

    Francesco-Giannini

--- github/week10/d5/progetto/src/app/pages/users/users.component.ts
+++ github/week10/d5/progetto/src/app/pages/users/users.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { TodosService } from '../../todos.service';
+import { iTodos } from '../../models/i-todos';
+import { iUsers } from '../../models/i-users';
+import { UsersService } from '../../users.service';
+
 
 @Component({
   selector: 'app-users',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+  todosArr:iTodos[]=[];
+  UsersArr:iUsers[]=[];
 
+  constructor(private UsersService:UsersService, private todosService:TodosService) {}
+
+  insieme(){
+    return this.todosArr.map(i=>{
+      const stessoid=this.UsersArr.find(a=>a.id===i.userId)
+      if (stessoid){i.author=stessoid}
+      return i
+    })
+  }
+
+    ngOnInit(){
+      this.todosArr=this.todosService.getTodos()
+      this.UsersArr=this.UsersService.getUsers()
+      this.insieme()
+      console.log(this.insieme())
+
+
+    }
 }
