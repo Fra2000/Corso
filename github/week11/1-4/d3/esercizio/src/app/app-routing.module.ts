7f439fc86afb190cb068e504fdd2526de0e01a07
Fra2000 <fra0000@gmail.com>
2024-06-06 16:21:51 +0200

    d3

---
+++ github/week11/1-4/d3/esercizio/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{ path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
