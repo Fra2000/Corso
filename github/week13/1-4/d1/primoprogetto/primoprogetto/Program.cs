119d2d0d5131af7408aa3821b7aa9434cfe2de43
Fra2000 <fra0000@gmail.com>
2024-06-17 17:04:45 +0200

    riallineamento

---
+++ github/week13/1-4/d1/primoprogetto/primoprogetto/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace primoprogetto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Atleta primoAtleta = new Atleta();
+            primoAtleta.Numero = 5;
+            primoAtleta.Nome = "Paolo";
+
+
+            Console.WriteLine(primoAtleta.Numero);
+            Console.WriteLine(primoAtleta.Nome);
+
+        }
+    }
+}
---
+++ github/week13/1-4/d1/primoprogetto/primoprogetto/atleta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primoprogetto
+{
+    internal class Atleta
+    {
+        string nome;
+        string cognome;
+        int numero;
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Cognome
+        {
+            get { return cognome; }
+            set { cognome = value; }
+        }
+    }
+}
