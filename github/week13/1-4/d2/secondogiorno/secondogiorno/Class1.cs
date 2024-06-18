f1e028b3ce57543c1428a60f214e6cd7e15271b4
Fra2000 <fra0000@gmail.com>
2024-06-18 16:42:41 +0200

    d2

---
+++ github/week13/1-4/d2/secondogiorno/secondogiorno/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondogiorno
+{
+    internal class Persona
+    {
+        private string nome;
+        private string cognome;
+        private byte eta;
+
+
+        public string getNome { get { return nome; } set { this.nome = value; } }
+        public string getCognome { get { return cognome; } set { this.cognome = value; } }
+        public byte getEta { get { return eta; } set { this.eta = value; } }
+
+        public Persona(string getNome, string getCognome, byte getEta)
+        {
+            this.nome = getNome;
+            this.cognome = getCognome;
+            this.eta = getEta;
+        }
+        public string getDescrizione()
+        {
+
+            return "Il nome di questa persona è " + nome + " ed il suo cognome è " + cognome + " di anni " + eta;
+        }
+    }
+}
---
+++ github/week13/1-4/d2/secondogiorno/secondogiorno/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace secondogiorno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Persona francesco = new Persona("Francesco", "Giannini",24);
+            string descrizione = francesco.getDescrizione();
+            Console.WriteLine(descrizione);
+        }
+    }
+}
