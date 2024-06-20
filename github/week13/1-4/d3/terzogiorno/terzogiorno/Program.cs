1ea96be74e15fdba1fb4cd40dc561c79f2ce8a32
Fra2000 <fra0000@gmail.com>
2024-06-21 00:40:23 +0200

    d3

--- github/week13/1-4/d3/terzogiorno/terzogiorno/Program.cs
+++ github/week13/1-4/d3/terzogiorno/terzogiorno/Program.cs
@@ -33,6 +33,55 @@ namespace terzogiorno
                 suoConto.ApriConto("1234", "Federico", 500);
             }
             catch (ArgumentException ex) { Console.WriteLine(ex.Message); }
+
+
+
+            string[] listanomi = { "francesco", "marco", "mario", "chiara", "lorenzo" };
+            string nome;
+            int i;
+            bool verifica = false;
+
+            Console.WriteLine("Inserire il nome da ricercare");
+            nome = Console.ReadLine();
+
+            for (i = 0; i < listanomi.Length; i++)
+            {
+                if (listanomi[i] == nome)
+                {
+                    Console.WriteLine("Il nome è presente nella lista");
+                    verifica = true;
+                }
+
+            }
+            if (verifica == false)
+            {
+                Console.WriteLine("Il nome non è presente nella lista");
+            }
+
+            
+
+
+
+            int x = 0;
+            Console.WriteLine("Specifica dimensione array");
+            x = int.Parse(Console.ReadLine());
+            int[] listanumeri = new int[x];
+
+            int e;
+            int somma = 0;
+
+            for (e = 0; e < listanumeri.Length; e++)
+            {
+                Console.WriteLine("Inserisci numero");
+                listanumeri[e] = int.Parse(Console.ReadLine());
+            }
+            for (e = 0; e < listanumeri.Length; e++)
+            {
+                somma += listanumeri[e];
+            }
+            Console.WriteLine($"La somma è: {somma}");
+            Console.WriteLine($"La media è: {somma / x}");
+            Console.ReadLine();
         }
     }
 }
