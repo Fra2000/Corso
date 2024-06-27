e2ef65d102ab3061245fba6825d923740a69303e
Fra2000 <fra0000@gmail.com>
2024-06-27 16:20:23 +0200

    d3

---
+++ github/week14/1-4/d3/D-3/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+using D_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace D_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// /////////////////////////////////////////////////////////////
+
+
+       
+
+        [HttpGet]
+      
+        public IActionResult Vendi()
+        {
+            return View(new Biglietto());
+        }
+
+        [HttpPost]
+        public IActionResult Vendi(Biglietto biglietto)
+        {
+            if (ModelState.IsValid)
+            {
+                var sala = CinemaData.Sale.FirstOrDefault(s => s.Nome == biglietto.Sala);
+                if (sala != null)
+                {
+                    if (sala.BigliettiTotali < sala.CapienzaMassima)
+                    {
+                        sala.BigliettiVenduti.Add(biglietto);
+                       
+                       TempData["Message"] = "Biglietto venduto con successo!";
+
+
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Non ci sono posti disponibili in questa sala.";
+                    }
+                }
+               
+            }
+          
+
+            return View(biglietto); // Passa il modello alla vista
+        }
+
+
+        /// /////////////////////////////////////////////////////////////////////
+
+        public IActionResult Index()
+        {
+            var sale = CinemaData.Sale ?? new List<Sala>();
+            return View(sale);
+        }
+
+        public IActionResult Riepilogo()
+        {
+            var biglietti = CinemaData.Sale?.SelectMany(s => s.BigliettiVenduti).ToList() ?? new List<Biglietto>();
+            return View(biglietti);
+        }
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ github/week14/1-4/d3/D-3/Models/Biglietto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D_3.Models
+{
+    public class Biglietto
+    {
+        [Required(ErrorMessage ="il nome è obbligatorio")]
+    public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "Il cognome è obbligatorio.")]
+        public string Cognome { get; set; }
+        [Required(ErrorMessage = "Seleziona una sala.")]
+        public string Sala { get; set; }
+
+        [Required(ErrorMessage = "Seleziona un tipo di biglietto.")]
+        public string Tipo  { get; set; }
+
+
+
+
+
+
+
+    }
+}
